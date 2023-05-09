d9d04d9b2d703fc6dfca3ddd941e7f38684cc7c5
jpaa0511 <110578643+jpaa0511@users.noreply.github.com>
2023-05-09 03:03:44 -0500

    S add urls d appVeter, view base y formRegistrarPersona, tambien

--- AppVeterinaria/views.py
+++ AppVeterinaria/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+def RegistrarPersona(request):
+    return render(request, 'RegistrarPersona.html')
