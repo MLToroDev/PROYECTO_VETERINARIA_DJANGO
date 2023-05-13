02bf70a7906a63ec0dbc2104f7cb7a20eb37be30
jpaa0511 <110578643+jpaa0511@users.noreply.github.com>
2023-05-12 21:36:53 -0500

    Se agregan views crud de persona, registro, mascota, factura. se agrega login, se configura archivos static, y se arreglan u optimizan etiquetas de todo el template

--- AppVeterinaria/urls.py
+++ AppVeterinaria/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registrar_mascota/', views.RegistrarMascota),
-    path('registrar_persona/', views.RegistrarPersona),
     path('', views.Inicio, name="inicio"),
+
+    path('registrar_persona/', views.RegistrarPersona),
+    path('listar_persona/', views.listarPersona),
+
+    path('signin/', views.Login)   
 ]
--- AppVeterinaria/views.py
+++ AppVeterinaria/views.py
@@ -5,9 +5,10 @@ from django.http import HttpResponse, JsonResponse
 
 def Inicio(request):
     return render(request, 'index.html')
-
+# Persona
 def RegistrarPersona(request):
-    return render(request, 'RegistrarPersona.html')
+    return render(request, 'Persona/RegistrarPersona.html')
 
-def RegistrarMascota(request):
-    return render(request, 'RegistrarMascota.html')
+def listarPersona(request):
+    return render(request, 'Persona/TablaPersona.html')
+
